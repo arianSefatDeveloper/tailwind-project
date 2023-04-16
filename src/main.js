2abd9b04952db3670fd26195238016bf1ab36614
mmdreza <m13r81a@gmail.come>
2023-04-17 00:58:04 +0430

    end of dark mode and end of project

--- src/main.js
+++ src/main.js
@@ -1,9 +1,30 @@
  const hamberger = document.querySelector("#hamberger"); 
  const mobileMenu = document.querySelector("#mobile-menu"); 
+ const dark = document.querySelector("#dark"); 
+ const darkMobile = document.querySelector("#dark-mobile"); 
+ const body = document.querySelector("body"); 
+ const mobileLinks = document.querySelectorAll("#mobile-link")
 
 
  hamberger.addEventListener("click",function(){
     mobileMenu.classList.toggle("hidden")
     hamberger.classList.toggle("text-white")
    
+ })
+
+
+ mobileLinks.forEach(function(link){
+   link.addEventListener("click" , function(){
+
+      mobileMenu.classList.toggle("hidden")
+    hamberger.classList.toggle("text-white")
+
+   })
+
+ })
+
+
+
+ dark.addEventListener("click",function(){
+   body.classList.toggle("dark")
  })
