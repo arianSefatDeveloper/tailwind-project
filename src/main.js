ad4511daaa24bb793335d75fff3fd62cf7889bd7
mmdreza <m13r81a@gmail.come>
2023-04-18 22:54:59 +0430

    editing

--- src/main.js
+++ src/main.js
@@ -4,11 +4,16 @@
  const darkMobile = document.querySelector("#dark-mobile"); 
  const body = document.querySelector("body"); 
  const mobileLinks = document.querySelectorAll("#mobile-link")
+ const line = document.querySelectorAll("#line")
 
 
  hamberger.addEventListener("click",function(){
     mobileMenu.classList.toggle("hidden")
     hamberger.classList.toggle("text-white")
+    line.forEach(function(itme){
+
+      itme.classList.toggle("bg-white")
+    })
    
  })
 
@@ -17,7 +22,7 @@
    link.addEventListener("click" , function(){
 
       mobileMenu.classList.toggle("hidden")
-    hamberger.classList.toggle("text-white")
+      hamberger.classList.toggle("text-white")
 
    })
 
